1c7b86db43d36d62c28cb6c6f282a32ea48075ce
antoapri <96533265+antoapri@users.noreply.github.com>
2022-01-04 19:13:45 +0700

    Update input_output.py

--- led/input_output.py
+++ led/input_output.py
@@ -3,19 +3,19 @@ import utime
 
 button = machine.Pin(20, machine.Pin.IN, machine.Pin.PULL_DOWN)
 led = machine.Pin(2, machine.Pin.OUT)
-led1 = machine.Pin(6, machine.Pin.OUT)
-led2 = machine.Pin(10, machine.Pin.OUT)
+led1 = machine.Pin(3, machine.Pin.OUT)
+led2 = machine.Pin(4, machine.Pin.OUT)
 
 button1 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led3 = machine.Pin(5, machine.Pin.OUT)
-led4 = machine.Pin(9, machine.Pin.OUT)
-led5 = machine.Pin(13, machine.Pin.OUT)
+led3 = machine.Pin(6, machine.Pin.OUT)
+led4 = machine.Pin(7, machine.Pin.OUT)
+led5 = machine.Pin(8, machine.Pin.OUT)
 
 button2 = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led6 = machine.Pin(26, machine.Pin.OUT)
-led7 = machine.Pin(27, machine.Pin.OUT)
-led8 = machine.Pin(28, machine.Pin.OUT)
-
+led6 = machine.Pin(10, machine.Pin.OUT)
+led7 = machine.Pin(11, machine.Pin.OUT)
+led8 = machine.Pin(12, machine.Pin.OUT)
+led9 = machine.Pin(13, machine.Pin.OUT)
 
 while True:
 #button ke1
@@ -29,25 +29,32 @@ while True:
         led.value(0)
         led1.value(0)
         led2.value(0)
+        
 #button ke2
     if button1.value() == 0:
          print("button 21")
+         led3.toggle()
+         led4.toggle()
+         led5.toggle()
          utime.sleep(0.1)
-         led3.value(3)
-         led4.value(3)
-         led5.value(3)
     else :
         led3.value(0)
         led4.value(0)
         led5.value(0)
+        
 #button ke3
     if button2.value() == 0:
          print("button 22")
+         led6.toggle()
          utime.sleep(0.1)
-         led6.value(1)
-         led7.value(1)
-         led8.value(1)
+         led7.toggle()
+         utime.sleep(0.1)
+         led8.toggle()
+         utime.sleep(0.1)
+         led9.toggle()
+         utime.sleep(0.1)
     else :
         led6.value(0)
         led7.value(0)
         led8.value(0)
+        led9.value(0)
